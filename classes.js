5aff848ba911453c0d87eb9beb916bf2abdf7e11
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-23 21:15:49 +0100

    Create example ES6 class
    * Constructor function adds properties
    * Add behaviour with other functions

---
+++ classes.js
@@ -0,0 +1,16 @@
+class Animal {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  speak() {
+    console.log("Hi, I'm " + this.name + " and I'm " + this.color)
+  }
+}
+
+let lion = new Animal("Mufasa", "golden brown");
+
+console.log(lion);
+
+lion.speak();

d9d2af072c8dc9ed3fe38769a6a7d25cd2c38c35
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-03 15:15:06 +0100

    Add array basics
    * Recap on basic array mutation

---
+++ array.js
@@ -0,0 +1,57 @@
+// create an array:
+
+let fruits = ['Apple', 'Strawberry'];
+
+console.log(fruits.length);
+
+// index into an array item:
+let first = fruits[0];
+let last = fruits[fruits.length - 1];
+
+// loop over an array:
+// the below returns the name of the item with it's index on each line:
+
+fruits.forEach(function(item, index, array) {
+  console.log(item, index);
+});
+
+// add to the front of an array
+// newLength is equal to the new array length, not the array itself
+let newLength = fruits.push('Orange');
+
+// remove from the end of an array:
+let lastItem = fruits.pop();
+
+// remove from the start of an array:
+// returns the removed item from the start of the array
+// the array has been mutated.
+// functional programming aims to treat all data as immutable
+let removeFirstItem = fruits.shift();
+
+// add to the front of an array:
+let addFirstItem = fruits.unshift('Blueberry');
+
+// find index of an array item:
+let position = fruits.indexOf('Blueberry');
+
+// remove a single item from an index position:
+// the second parameter is the number of items to be removed
+let removeItem = fruits.splice(position, 1);
+
+// remove more than one item from an index position:
+let vegetables = ['Cabbage', 'Kale', 'Mushrooms', 'Onions'];
+console.log(vegetables);
+
+let position = 1, n = 2;
+
+// n defines the number of items to be removed.
+let removedItems = vegetables.splice(position, n);
+
+// you now have two separate arrays
+// the first two items are still in the original vegetables array
+// the second two items have been removed and are now in a new removedItems array
+// this is mutating data - so not so functional
+
+// make a copy of an array:
+let shallowCopy = vegetables.slice();
+// now shallowCopy and vegetables hold the same elements in an array

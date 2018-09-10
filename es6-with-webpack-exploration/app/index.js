3aaa64c2a883df3f81a4fc2361af70ea30884ed4
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-10 14:41:21 +0100

    Add simple weather app with api call
    * Handle promises following fetch

--- es6-with-webpack-exploration/app/index.js
+++ es6-with-webpack-exploration/app/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const api_key = '';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: 'London',
+      description: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getWeather(this.state.city)
+  }
+
+  getWeather(city) {
+    // the .then is handling of the promises that are returned by each function
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({description: json.weather[0].description})
+      })
+  }
+
   render() {
     return (
-      <div>React is working with JSX</div>
+      <div>
+        <h1>Weather report for {this.state.city}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
     )
   }
 }

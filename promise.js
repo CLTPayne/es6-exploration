142e543fd7aeedc0dc8b1b9121e41822dc057442
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-11 11:01:31 +0100

    Add promise definition and Promise.all()

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 // a promise is a special javascript object
+
+// DEFINITION:
+// The promise object is used for deferred and asynchronous computations.
+// A promise represents an operation that hasn't completed yet, but is expected
+
 // involves producing code - something that takes some time like a script
 // consuming code - functions that that need the result of the producing code
-// promise connects the aboce two
+// promise connects the above two
 // the function passed to a promise is called an executor.
 
 // constructor syntax for a promise is:
@@ -24,3 +29,27 @@ let promise = new Promise(function(resolve, reject) {
 let promise = new Promise(function(resolve, reject) {
   setTimeout(() => resolve('done!'), 1000);
 });
+
+example promise:
+
+function stats (file) {
+  return new Promise((resolve, reject) => {
+    fs.stat(file, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(data)
+    })
+  })
+}
+
+Promise.all([
+  stats('file1'),
+  stats('file2'),
+  stats('file3')
+])
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
+
+// Promise.all() is an iterable method that returns a single Promise
+// that resolves when all of the promises in the iterable argument have resolved.

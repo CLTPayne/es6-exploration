082a9e2c6130ef0c3e6a4a5b70f7160e54b4f913
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-10-08 22:19:41 +0100

    Demo simple num and string type conversion

---
+++ typeConversion.js
@@ -0,0 +1,11 @@
+const toNumber = + '100';
+
+const toString = '' + 100;
+
+console.log(toNumber) // 100
+
+console.log(typeof toNumber) // number
+
+console.log(toString) // '100'
+
+console.log(typeof toString) // string

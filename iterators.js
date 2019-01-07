a97c186027c77bb2bffabe5c8f1a136a03db5df1
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-01-07 22:45:10 +0000

    Add second iterator with next() method on object

--- iterators.js
+++ iterators.js
@@ -15,3 +15,23 @@ console.log(myIterator()); // 'a'
 console.log(myIterator()); // 'b'
 console.log(myIterator()); // 'c'
 console.log(myIterator()); // 'd'
+
+
+function nextIterator(arr) {
+  let i = 0;
+  const returnElement = {
+    next: function() {
+      let element = arr[i];
+    	i++;
+    	return element;
+    }
+  }
+	return returnElement;
+}
+
+const array2 = [1, 2, 3];
+const iteratorWithNext = nextIterator(array2);
+
+console.log(iteratorWithNext.next()); // 1
+console.log(iteratorWithNext.next()); // 2
+console.log(iteratorWithNext.next()); // 3

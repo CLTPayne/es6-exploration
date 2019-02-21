94b2d06ffbea910a2adf57313ee1cef787c5d49e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-21 21:36:54 +0000

    Fix vulnerabilities

--- es6-with-webpack-exploration/webpack.config.js
+++ es6-with-webpack-exploration/webpack.config.js
@@ -9,13 +9,13 @@ module.exports = {
   devServer: {
     port: 3000,
     contentBase: path.resolve(__dirname, 'build')
-  },
+  }
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
+        exclude: /node_modules/
+        use: ['babel']
       }
     ]
   }

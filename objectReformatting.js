21f3b433930da8fafc9a84c4e7a0279cbd3c522b
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-06-10 00:33:03 +0100

    Refactor to use reduce() instead of for/of

--- objectReformatting.js
+++ objectReformatting.js
@@ -5,6 +5,11 @@ const input = [
         age: 59
     },
     {
+        name: "William Who",
+        profession: "Actor",
+        age: 16
+    },
+    {
         name: "Sarah Sue",
         profession: "Software Engineer",
         age: 29
@@ -13,11 +18,6 @@ const input = [
         name: "Jane Joe",
         profession: "Actor",
         age: 34
-    },
-    {
-        name: "William Who",
-        profession: "Actor",
-        age: 16
     },
     {
         name: "Indie I",
@@ -42,7 +42,24 @@ const groupPeopleByProfession = people => {
     return grouped;
 }
 
-console.log(groupPeopleByProfession(input))
+// console.log(groupPeopleByProfession(input))
 
-{ Actor: [ 'Billy Bob', 'Jane Joe', 'William Who' ],
-  'Software Engineer': [ 'Sarah Sue', 'Indie I' ] }
+// { Actor: [ 'Billy Bob', 'Jane Joe', 'William Who' ],
+//   'Software Engineer': [ 'Sarah Sue', 'Indie I' ] }
+
+const refactorGroupPeopleByProfession = people => {
+    return people.reduce((grouped, person) => {
+        if (!grouped[person.profession]) {
+            grouped[person.profession] = [
+                person.name
+            ]
+        } else {
+            grouped[person.profession].push(
+                person.name
+            )
+        }
+        return grouped;
+    }, {})
+}
+
+console.log(refactorGroupPeopleByProfession(input))

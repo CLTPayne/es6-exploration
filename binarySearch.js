55f1da2c851f5a97d33ecbd1792c7c29e778d80c
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-07-14 13:21:01 +0100

    Recursive binary search

--- binarySearch.js
+++ binarySearch.js
@@ -9,4 +9,21 @@ const findIndex = (array, item) => {
     }
 }
 
-console.log(findIndex([1, 2, 3, 4, 5], 2))
+// console.log(findIndex([1, 2, 3, 4, 5], 2))
+
+const findItem = (sortedArray, start, end, item) => {
+    let middle = Math.floor((start + end) / 2);
+    if (sortedArray[middle] === item) {
+        return middle;
+    }
+    if (item < sortedArray[middle]) {
+        return findItem(sortedArray, start, middle - 1, item);
+    }
+    if (item > sortedArray[middle]) {
+        return findItem(sortedArray, middle + 1, end, item);
+    }
+
+}
+
+const array = [1, 7, 8, 9, 11, 12, 20, 200]
+console.log(findItem(array, 0, array.length, 20))
